218183fb81dd5f7e3478b46450b6292799cc52fc
shawn.thecool <shawn.thecool@kakaocorp.com>
2018-12-28 17:40:12 +0900

    UPDATE config exchanger.js

--- src/config/exchanger.js
+++ src/config/exchanger.js
@@ -1,38 +1,55 @@
-const JSTL_CONTENT_URL = '${content_url}';
-const ROOT_PATH = '../view/';
-const TARGET_PATH = ROOT_PATH + cmdArgs.path;
-
 const cmdArgs = require('shell-arguments');
+const pipeline = require('node-pipeline');
 const fs = require('fs');
 const path = require('path');
-const config = require(TARGET_PATH);
+const config = require('../view/' + cmdArgs.path);
+const filePath = path.join(__dirname, '../view/index.html');
 
-const filePath = path.join(__dirname, TARGET_PATH)
-
-function exchanger(config) {
-  let filePath = path.join(__dirname, '..', cmdPath)
-  let filedata = null;
-  config.images.map((img) => {
-    const kageUrl = JSTL_CONTENT_URL + img.kageUrl
-    const changeImg = {
-      'DEV': () => {
-        try { 
-          data = fs.readFileSync(filePath, 'utf8') 
-        } catch (err) { console.log(err); }
-      },
-      'REAL': () => {
-        filedata = fs.readFileSync(filePath, 'utf8').replace(img.localUrl, kageUrl)
-      }
-    }
-    changeImg[config.env || 'DEV']();
-  })
-
-  return () => {
-    try { fs.writeFileSync(filePath, filedata, 'utf8'); } catch (err) { console.log(err); }
-  }
+function _replaceAll(str, asis, tobe) {
+  const regex = new RegExp(asis, 'g');
+  return str.replace(regex, tobe);
 }
 
-// exchanger(config)()
+function getFile(filePath, next) {
+  try {
+    next(fs.readFileSync(filePath, 'utf8'));
+  } catch (err) { console.log(err); }
+}
+
+function setFileData(filePath, data) {
+  try {
+    fs.writeFileSync(filePath, data, 'utf8');
+  } catch (err) { console.log(err); }
+}
+
+// pipeline
+getFile(filePath, (data) => {
+  const pl = pipeline.create('html');
+
+  pl.on('end', function (err, results) {
+    if (err) { console.log('Error in pipeline: ' + err); }
+
+    const convertedData = results.filter(process => process.replacedImage);
+    if (convertedData.length > 0) { setFileData(filePath, convertedData[0].replacedImage); }
+    process.exit();
+  });
+
+  // add process
+  pl.use((results, next) => {
+    let html = results[0].html
+    config.images.map((img) => {
+      html = _replaceAll(html, img.localUrl, img.kageUrl)
+    });
+    next(null, { replacedImage: html });
+  });
+
+  pl.execute({ html: data });
+
+});
+
+
+
+
 
 
 
