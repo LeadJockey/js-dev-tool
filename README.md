1b4438a3d4c21936c6e68cf8561927f43bd3157d
shawn.thecool <shawn.thecool@kakaocorp.com>
2019-01-04 15:54:10 +0900

    UPDATE fil-structure maker

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # js-dev-tool
-.
+
+kakaopage 찰리툴 로컬 개발용 툴
+
+node 8 버전 이상 지원
+
+## command line use age
+```
+npm run create -- --dir={파일명}
+npm run create
+```
--- src/config/createFiles.js
+++ src/config/createFiles.js
@@ -1,12 +1,69 @@
 const cmdArgs = require('shell-arguments');
-const dir = cmdArgs.dir;
+const tmplate = require('./tmeplates');
+const fs = require('fs');
+const path = require('path');
+const dirname = cmdArgs.dir || 'temp';
+const files = cmdArgs.files;
 
-console.log('d');
+function createDir(rootPath, dirname) {
+  const dirPath = path.join(rootPath, dirname);
 
-console.log('dir', dir);
+  if (fs.existsSync(dirPath)) return;
 
+  fs.mkdirSync(dirPath);
+}
 
-// if (!fs.existsSync(dir)){
-//   fs.mkdirSync(dir);
-// }
+function createFile(dirPath, filename, content) {
+  const filePath = path.join(dirPath, filename);
+  const fileContent = content || '';
 
+  if (fs.existsSync(filePath)) return;
+
+  fs.writeFileSync(filePath, fileContent);
+}
+
+function getTemplate(opts) {
+  return `
+  <head>
+    ${opts.meta || ''}
+    ${opts.socialShareMeta || ''}
+    
+    <style>
+      ${opts.reset || ''}
+    </style>
+
+  </head>
+
+  ${opts.layout || ''}
+
+  `;
+}
+
+async function init() {
+  if (!dirname) return;
+  const rootPath = path.join(__dirname, '..', 'view');
+  const dirPath = path.join(rootPath, dirname);
+  const templatePath = path.join(dirPath, 'templates');
+  await createDir(rootPath, dirname);
+  await createDir(dirPath, 'images');
+  await createDir(dirPath, 'templates');
+  await createFile(dirPath, 'README.md', tmplate.getREADMETemplate());
+  await createFile(dirPath, 'config.json', tmplate.getDefaultCONFIGFormat());
+  await createFile(templatePath, 'pc.ftl', getTemplate({
+    meta: tmplate.getDefaultMetaTemplate(),
+    reset: tmplate.getPCResetCssTemplate(),
+    layout: tmplate.getDefaultLayoutTemplate()
+  }));
+  await createFile(templatePath, 'mo.ftl', getTemplate({
+    meta: tmplate.getDefaultMetaTemplate(),
+    reset: tmplate.getMobileResetCssTemplate(),
+    layout: tmplate.getDefaultLayoutTemplate()
+  }));
+  await createFile(templatePath, 'app.ftl', getTemplate({
+    meta: tmplate.getDefaultMetaTemplate(),
+    reset: tmplate.getMobileResetCssTemplate(),
+    layout: tmplate.getDefaultLayoutTemplate()
+  }));
+
+}
+init();
